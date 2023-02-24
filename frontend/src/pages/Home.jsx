edf5795e120d0496a4b7e4cb7918b27307b54de5
Sayantan Kundu <sayantankundu02@gmail.com>
2023-02-24 19:23:44 +0530

    setting up frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import {Menu, Profile, ConnectN, RightN} from "../components/node_comp";
+// import '../css/node_css/index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Node() {
+    
+    return (
+        <div>
+            home
+        </div>
+    )
+}
